b3fa0ea7dc57949ab890c8c7a591ca9c694c69d4
Ugochukwu <ugopeter26@gmail.com>
2022-10-04 00:05:56 +0100

    samee

--- controllers/crud.js
+++ controllers/crud.js
@@ -13,12 +13,18 @@ exports.getIndex = async (req, res) => {
 exports.getMyProfile = async (req, res) => {
   try {
     const user = await User.findById(req.userId);
-    const followingCount = 2;
-    const followersCount = 500;
-    const backdropUrl =
-      "https://res.cloudinary.com/dtgigdp2j/image/upload/v1664820980/profileImages/c8otiaauqetohvrdpq2z.jpg";
-    const displayUrl =
-      "https://res.cloudinary.com/dtgigdp2j/image/upload/v1664821014/profileImages/dkqvt00s5i5n70djouc5.jpg";
+    let followingCount = Math.floor(Math.random() * 1000);
+    let followersCount = Math.floor(Math.random() * 1000);
+    let backdropUrl = "https://placeimg.com/640/360/any";
+    let displayUrl = "https://placeimg.com/640/360/any";
+    if (user.username === "HoodieDan") {
+      backdropUrl =
+        "https://res.cloudinary.com/dtgigdp2j/image/upload/v1664820980/profileImages/c8otiaauqetohvrdpq2z.jpg";
+      displayUrl =
+        "https://res.cloudinary.com/dtgigdp2j/image/upload/v1664821014/profileImages/dkqvt00s5i5n70djouc5.jpg";
+      followingCount = 2;
+      followersCount = 500;
+    }
     // const bio = ""
     let { name, username, stack, location, email, bio, verified } = user;
     if (!bio) {
