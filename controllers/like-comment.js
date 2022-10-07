733bdb6d749163d3c167b2a5130edf8797880b13
Ugochukwu <ugopeter26@gmail.com>
2022-10-07 11:34:03 +0100

    updating..

--- controllers/like-comment.js
+++ controllers/like-comment.js
@@ -8,7 +8,6 @@ const extractCommentToSend = (comment) => {
     commentId: comment.id,
     authorId: comment.author.id,
     postId: comment.post,
-
     username: comment.author.username,
     authorImage: comment.author.displayUrl,
     isVerified: comment.author.verified,
@@ -18,6 +17,10 @@ const extractCommentToSend = (comment) => {
   };
 };
 
+const extractLikersInfo = (likes) => {
+  infosToReturn = [];
+  let infoToReturn = {};
+};
 exports.postComment = async (req, res) => {
   const userId = req.userId;
   const { postId, body } = req.body;
@@ -64,6 +67,73 @@ exports.getCommentsFromPostId = async (req, res) => {
     });
     commentsToSend.reverse();
     res.status(200).json({ comments: commentsToSend });
+  } catch (err) {
+    catchError(err, res);
+  }
+};
+
+exports.likePost = async (req, res) => {
+  const postId = req.query.postId;
+  const loggedInUserId = req.userId;
+  const isValid = isValidObjectId(postId);
+  try {
+    if (!isValid) {
+      const error = new Error("Errmm. postId is a invalid ");
+      error.statusCode = 401;
+      throw error;
+    }
+    const post = await Post.findById(postId);
+    if (!post.likes.includes(loggedInUserId)) {
+      post.likes.push(loggedInUserId);
+    }
+    post.save();
+    res.status(200).json({ message: "Liked!" });
+  } catch (err) {
+    catchError(err, res);
+  }
+};
+
+exports.unLikePost = async (req, res) => {
+  const postId = req.query.postId;
+  const loggedInUserId = req.userId;
+  const isValid = isValidObjectId(postId);
+  try {
+    if (!isValid) {
+      const error = new Error("Errmm. postId is a invalid ");
+      error.statusCode = 401;
+      throw error;
+    }
+    const post = await Post.findById(postId);
+    if (post.likes.includes(loggedInUserId)) {
+      const position = post.likes.indexOf(loggedInUserId);
+      post.likes.splice(position, 1);
+    }
+    post.save();
+
+    res.status(200).json({ message: "UnLiked!" });
+  } catch (err) {
+    catchError(err, res);
+  }
+};
+
+exports.getLikers = async (req, res) => {
+  const postId = req.query.postId;
+  const loggedInUserId = req.userId;
+  const isValid = isValidObjectId(postId);
+  try {
+    if (!isValid) {
+      const error = new Error("Errmm. postId is a invalid oga");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const post = await Post.findById(postId).populate({
+      path: "likes",
+      model: "User",
+    });
+    const likers = post.likes;
+
+    res.json({ postId });
   } catch (err) {
     catchError(err, res);
   }
