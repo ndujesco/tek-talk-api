780e5a6d6680eaf310942346d9bc29aa68038690
Ugochukwu <ugopeter26@gmail.com>
2022-10-14 00:33:22 +0100

    update...

--- models/notification.js
+++ models/notification.js
@@ -1,20 +1,25 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const notificationSchema = new Schema({
-  class: String, //like, comment, follow, mention
-  mentionLocation: String,
-  seen: Boolean,
-  postId: String,
-  userId: String,
-  postId: String,
-  username: String,
-  name: String,
-  count: { type: Number, default: 0 },
-  loggedUserId: String,
-  postBody: String,
-  commentBody: String,
-  followersId: [String],
-  commentId: String,
-});
+const notificationSchema = new Schema(
+  {
+    class: String, //like, comment, follow, mention
+    mentionLocation: String,
+    seen: Boolean,
+    postId: String,
+    userId: String,
+    postId: String,
+    username: String,
+    name: String,
+    count: { type: Number, default: 0 },
+    loggedUserId: String,
+    postBody: String,
+    commentBody: String,
+    followersId: [String],
+    commentId: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 exports.Notification = mongoose.model("Notification", notificationSchema);
