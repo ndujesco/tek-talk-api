a8e66cad82f57e72ca6273c092b1776ee2ce0d6f
Ugochukwu <ugopeter26@gmail.com>
2022-10-14 14:28:02 +0100

    update

--- utils/send-mail.js
+++ utils/send-mail.js
@@ -22,9 +22,9 @@ exports.sendEmail = async (details) => {
       </head>
       <body style="font-family: sans-serif; text-align: center; color: #000" >
         <div style="display: block; margin: auto; max-width: 600px;" class="main">
-          <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px ">Link expires after an hour</h1>
+          <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px ">Link expires after 30 minutes.</h1>
           <div> 
-                <p><a class="button-58" role="button" style=" background-color: #06f; color: white; padding: 12px 20px; text-decoration: none;" href="${details.url}?token=${details.token}">Reset Password</a></p>
+                <p><a class="button-58" role="button" style=" background-color: #06f; color: white; padding: 12px 20px; text-decoration: none;" href="https://tektalk.vercel.app/reset-password?token=${details.token}">Reset Password</a></p>
           </div>
         </div>
         <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
