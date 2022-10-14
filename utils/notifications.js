231fb49836e56bca61fedd8a617f64aebab619c0
Ugochukwu <ugopeter26@gmail.com>
2022-10-14 08:00:28 +0100

    update...

--- utils/notifications.js
+++ utils/notifications.js
@@ -116,16 +116,3 @@ exports.notifyFollow = async (userToFollow, loggedInUser) => {
 
   notification.save();
 };
-
-// class: String, //like, comment, follow, mention
-// mentionLocation: String,
-// seen: Boolean,
-// postId: String,
-// userId: String,
-// postId: String,
-// username: String,
-// name: String,
-// count: { type: Number, default: 0 },
-// loggedUserId: String,
-// postBody: String,
-// commentBody: String,
