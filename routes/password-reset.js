2e5ca5dc9f50cbde9931b6c342b7678d67213193
Ugochukwu <ugopeter26@gmail.com>
2022-10-16 21:54:52 +0100

    update

--- routes/password-reset.js
+++ routes/password-reset.js
@@ -23,19 +23,22 @@ router.patch("/change-password", isAuthenticated, changePassword);
 
 router.post("/post-talk", async (req, res) => {
   const { name, description, displayUrl } = req.body;
+  let message = "New!";
   if (!name || !description || !displayUrl)
     return res
       .status(422)
       .json({ status: 422, message: "Some fields missing boss" });
 
-  const isTalk = await Talk.findOne({ name });
-  if (isTalk)
-    return res
-      .status(400)
-      .json({ status: 400, message: name + " already exists boss" });
-  const talk = new Talk({ ...req.body });
+  let talk = await Talk.findOne({ name });
+  if (talk) {
+    talk.name = name;
+    talk.displayUrl = displayUrl;
+    talk.description = description;
+    message = "Modified!";
+  }
+  talk = new Talk({ ...req.body });
   talk.save();
-  res.status(200).json({ status: 200, talk });
+  res.status(200).json({ status: 200, message });
 });
 
 module.exports = router;
