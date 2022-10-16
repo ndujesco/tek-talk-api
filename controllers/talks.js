54cf1e7baf501dcf27b7996ec7b2c143469e3d4a
Ugochukwu <ugopeter26@gmail.com>
2022-10-16 20:25:17 +0100

    update

--- controllers/talks.js
+++ controllers/talks.js
@@ -4,6 +4,7 @@ const { catchError } = require("../utils/help-functions");
 exports.getTalks = async (req, res) => {
   try {
     const talks = await Talk.find();
+    return res.status(200).json({ status: 200, talks });
   } catch (err) {
     catchError(err, res);
   }
